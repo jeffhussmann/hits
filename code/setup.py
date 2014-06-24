de10ccd4c155ba80e0a82f7fc04f90c5faa23f82
Jeff Hussmann <jeff.hussmann@gmail.com>
2014-06-23 19:56:10 -0500

    added adapters_cython.pyx

--- code/setup.py
+++ code/setup.py
@@ -4,13 +4,8 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 include_dirs = [np.get_include()]
-ext_modules = [#Extension('adapters_cython', ['Circles/adapters_cython.pyx'], include_dirs=include_dirs),
-               #Extension('amplicons_cython', ['Circles/amplicons_cython.pyx'], include_dirs=include_dirs),
-               #Extension('barcodes_cython', ['Circles/barcodes_cython.pyx'], include_dirs=include_dirs),
-               #Extension('periodicity_cython', ['Circles/periodicity_cython.pyx'], include_dirs=include_dirs),
-               #Extension('bayes_base_call_cython', ['Circles/bayes_base_call_cython.pyx'], include_dirs=include_dirs),
+ext_modules = [Extension('adapters_cython', ['Sequencing/adapters_cython.pyx'], include_dirs=include_dirs),
                Extension('fastq_cython', ['Sequencing/fastq_cython.pyx'], include_dirs=include_dirs),
-               #Extension('seed_extend_cython', ['Circles/seed_extend_cython.pyx'], include_dirs=include_dirs),
               ]
 
 setup(
