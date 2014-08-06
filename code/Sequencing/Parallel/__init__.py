8fc39b252c173a7b8c71200b353fa110125d86f9
Jeff Hussmann <jeff.hussmann@gmail.com>
2014-08-05 21:38:24 -0500

    added whole thing option to piece_of_list

--- code/Sequencing/Parallel/__init__.py
+++ code/Sequencing/Parallel/__init__.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 def piece_of_list(full_list, num_pieces, which_piece):
+    if which_piece == -1:
+        # Sentinel value that means 'the whole thing'.
+        return full_list
+
     base_size = len(full_list) // num_pieces
     num_with_extra = len(full_list) - base_size * num_pieces
     sizes = np.ones(num_pieces, int) * base_size
