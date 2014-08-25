f7a5149abb3bd0a9b34ac660704761d195927a7f
Jeff Hussmann <jeff.hussmann@gmail.com>
2014-08-25 09:51:16 -0500

    transparent handling of tuples as keys in counts

--- code/Sequencing/Serialize/counts.py
+++ code/Sequencing/Serialize/counts.py
@@ -2,14 +2,16 @@ from collections import Counter
 
 def write_file(counter, file_name):
     with open(file_name, 'w') as fh:
-        for string, count in counter.most_common():
-            fh.write('{0}\t{1}\n'.format(string, count))
+        for key, count in counter.most_common():
+            if isinstance(key, tuple):
+                key = ' '.join(key)
+            fh.write('{0}\t{1}\n'.format(key, count))
 
 def read_file(file_name):
     counter = Counter()
     for line in open(file_name):
-        string, count = line.strip().split()
-        counter[string] = int(count)
+        key, count = line.strip().split('\t')
+        counter[key] = int(count)
     return counter
 
 def combine_data(first_counter, second_counter):
