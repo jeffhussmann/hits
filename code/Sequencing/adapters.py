7cf497e2bf64ca2328ada4c4528dcac7e3733fbc
Jeff Hussmann <jeff.hussmann@gmail.com>
2014-09-17 17:04:58 -0500

    adds build_adapter_ranges

--- code/Sequencing/adapters.py
+++ code/Sequencing/adapters.py
@@ -1,4 +1,5 @@
 from adapters_cython import *
+import numpy as np
 
 tru_seq_R1_rc = 'AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGA'
 tru_seq_R2_rc = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC'
@@ -17,6 +18,35 @@ def build_adapters(index_sequence='', max_length=None):
     adapter_in_R2 = tru_seq_R1_rc + P5_rc + A_tail
     truncated_slice = slice(None, max_length)
     return adapter_in_R1[truncated_slice], adapter_in_R2[truncated_slice]
+
+def build_adapter_ranges(index_sequence):
+    def make_ranges(construct, names):
+        cumulative_lengths = list(np.cumsum(map(len, construct)))
+        bounds = zip([0] + cumulative_lengths, cumulative_lengths)
+        ranges = zip(names, bounds)
+        return ranges
+    
+    R1_construct = [tru_seq_R2_rc, index_sequence, P7_rc, A_tail]
+    R1_names = ['TruSeq R2', 'I7', 'P7', 'A tail']
+
+    chemistry_only_cycles = 7
+    I5_length = 8
+    R2_construct = [tru_seq_R1_rc[:-(I5_length + chemistry_only_cycles)],
+                    tru_seq_R1_rc[-(I5_length + chemistry_only_cycles):-(chemistry_only_cycles)],
+                    tru_seq_R1_rc[-chemistry_only_cycles:],
+                    P5_rc,
+                    A_tail,
+                   ]
+    R2_names = ['TruSeq R1',
+                'I5',
+                'Chemistry only',
+                'P7',
+                'A tail',
+               ]
+    
+    R1_ranges = make_ranges(R1_construct, R1_names)
+    R2_ranges = make_ranges(R2_construct, R2_names)
+    return R1_ranges, R2_ranges
 
 def get_barcode_ranges(index_sequence):
     start = len(tru_seq_R2_rc)
