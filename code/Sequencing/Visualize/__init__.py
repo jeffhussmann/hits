f49ac96193f555fe42bf36d7ab14f04ca4743309
Jeff Hussmann <jeff.hussmann@gmail.com>
2015-01-16 15:51:34 -0600

    additions to Visualize.__init__

--- code/Sequencing/Visualize/__init__.py
+++ code/Sequencing/Visualize/__init__.py
@@ -1,3 +1,9 @@
+import igv_colors
+import scipy.stats
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
 def optional_ax(original_function):
     def possibly_new_ax(*args, **kwargs):
         ax_given = kwargs.get('ax')
@@ -16,3 +22,73 @@ def optional_ax(original_function):
         return result
     
     return possibly_new_ax
+
+def add_commas_to_yticks(ax):
+    def commas_formatter(x, pos):
+        return '{0:,}'.format(int(x))
+    tick_formatter = matplotlib.ticker.FuncFormatter(commas_formatter)
+    ax.yaxis.set_major_formatter(tick_formatter)
+
+def enhanced_scatter(x_list,
+                     y_list,
+                     x_label,
+                     y_label,
+                     title,
+                     ax_scatter,
+                     lims=None,
+                     color_by_density=True,
+                     do_fit=True,
+                    ):
+    same_lists = np.allclose(x_list, y_list)
+
+    if color_by_density and not same_lists:
+        sampled_points = np.vstack([x_list[:10000], y_list[:10000]])
+        points = np.vstack([x_list, y_list])
+        kernel = scipy.stats.gaussian_kde(sampled_points)
+        colors = kernel(points)
+    else:
+        colors = np.ones_like(x_list)
+
+    if same_lists:
+        do_fit = False
+
+    kwargs = {'cmap': matplotlib.cm.jet,
+              's': 4,
+              'linewidths' : (0.1,),
+             }
+
+    ax_scatter.scatter(x_list, y_list, c=colors, **kwargs)
+
+    ax_scatter.set_xlabel(x_label)
+    ax_scatter.set_ylabel(y_label)
+
+    ax_scatter.set_title(title)
+
+    if do_fit:
+        fit = np.polyfit(x_list, y_list, 1)
+        beta, _ = fit
+        fit_function = np.poly1d(fit)
+        xs = ax_scatter.get_xlim()
+        ax_scatter.plot(xs, fit_function(xs), color='black', alpha=0.5)
+        ax_scatter.set_xlim(min(x_list), max(x_list))
+        
+        r, p = scipy.stats.pearsonr(x_list, y_list)
+        ax_scatter.annotate('r = {:0.2f}, p={:0.2e}'.format(r, p),
+                            xy=(1, 0),
+                            xycoords='axes fraction',
+                            xytext=(-10, 15),
+                            textcoords='offset points',
+                            horizontalalignment='right',
+                           )
+        ax_scatter.annotate(r'$\beta$ = {:0.2f}'.format(beta),
+                            xy=(1, 0),
+                            xycoords='axes fraction',
+                            xytext=(-10, 30),
+                            textcoords='offset points',
+                            horizontalalignment='right',
+                           )
+    
+    ax_scatter.set_aspect(1.)
+    if lims:
+        ax_scatter.set_xlim(*lims)
+        ax_scatter.set_ylim(*lims)
