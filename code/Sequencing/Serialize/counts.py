40427356b36cd57f3e220d42aeb56197f14be809
Jeff Hussmann <jeff.hussmann@gmail.com>
2015-01-16 15:48:54 -0600

    Serialize.counts now deals in dictionaries of counters

--- code/Sequencing/Serialize/counts.py
+++ code/Sequencing/Serialize/counts.py
@@ -1,19 +1,33 @@
 from collections import Counter
 
-def write_file(counter, file_name):
+def write_file(dict_of_counters, file_name):
     with open(file_name, 'w') as fh:
-        for key, count in counter.most_common():
-            if isinstance(key, tuple):
-                key = ' '.join(key)
-            fh.write('{0}\t{1}\n'.format(key, count))
+        for name, counter in sorted(dict_of_counters.items()):
+            if '\t' in name:
+                raise ValueError('Name of counter can\'t contain a tab')
+            fh.write('{0}\n'.format(name))
+
+            for key, count in counter.most_common():
+                if isinstance(key, tuple):
+                    key = ' '.join(key)
+                fh.write('{0}\t{1}\n'.format(key, count))
 
 def read_file(file_name):
+    dict_of_counters = {}
     counter = Counter()
     for line in open(file_name):
-        key, count = line.strip().split('\t')
-        counter[key] = int(count)
-    return counter
+        line = line.strip()
+        if '\t' not in line:
+            name = line
+            counter = Counter()
+            dict_of_counters[name] = counter
+        else:
+            key, count = line.split('\t')
+            counter[key] = int(count)
 
-def combine_data(first_counter, second_counter):
-    first_counter.update(second_counter)
-    return first_counter
+    return dict_of_counters
+
+def combine_data(first, second):
+    for name in second:
+        first[name].update(second[name])
+    return first
