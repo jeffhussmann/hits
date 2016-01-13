be88a384121d09754b67a1bd909f3c8487a0c65d
Jeff Hussmann <jeff.hussmann@gmail.com>
2016-01-13 12:43:50 -0800

    adds binary_widget

--- code/Sequencing/ipython.py
+++ code/Sequencing/ipython.py
@@ -1,3 +1,5 @@
+import ipywidgets
+
 toggle = '''
 <script>
 code_show=true; 
@@ -13,3 +15,8 @@ $( document ).ready(code_toggle);
 </script>
 To toggle code, click <a href="javascript:code_toggle()">here</a>.
 '''
+
+def binary_widget(default=True):
+    values = [int(default), int(not default)]
+    labels = [str(default), str(not default)]
+    return ipywidgets.RadioWidget(values, labels=labels)
