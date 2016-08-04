b31ceb13e15a3e87968d1a70fa72800e07f8926b
Jeff Hussmann <jeff.hussmann@gmail.com>
2016-08-04 12:30:09 -0700

    always make fasta seqs upper case

--- code/Sequencing/fasta.py
+++ code/Sequencing/fasta.py
@@ -11,6 +11,5 @@ Read.__str__ = Read_to_record
 def reads(file_name):
     ''' Yields the name and sequence lines from a fasta file. '''
     for record in Bio.SeqIO.parse(file_name, 'fasta'):
-        read = Read(record.name, str(record.seq))
+        read = Read(record.name, str(record.seq).upper())
         yield read
-
