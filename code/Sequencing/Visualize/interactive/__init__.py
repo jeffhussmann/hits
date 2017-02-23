c735e3ef3ea6db11576710e60114e375fc87d3ad
Jeff Hussmann <jeff.hussmann@gmail.com>
2017-02-22 22:02:41 -0800

    typo in docstring

--- code/Sequencing/Visualize/interactive/__init__.py
+++ code/Sequencing/Visualize/interactive/__init__.py
@@ -80,7 +80,7 @@ def scatter(df,
                 If 'color' is a column, it will be used to color the points. 
                 Index values will be used as labels for points.
                 Any text columns will be searchable through the 'Search:' field.
-                Any boolean columns will be used to define subset of points for
+                Any boolean columns will be used to define subsets of points for
                 selection from a dropdown menu.
             hover_keys: Names of columns in df to display in the tooltip that appears
                 when you hover over a point.
