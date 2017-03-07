b46be174696b71cdde24b4184e35f381bf7ab17f
Jeff Hussmann <jeff.hussmann@gmail.com>
2017-03-07 14:22:22 -0800

    makes installable

--- code/setup.py
+++ code/setup.py
@@ -4,6 +4,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 include_dirs = [np.get_include()]
+
 ext_modules = [Extension('adapters_cython', ['Sequencing/adapters_cython.pyx'], include_dirs=include_dirs),
                Extension('fastq_cython', ['Sequencing/fastq_cython.pyx'], include_dirs=include_dirs),
                Extension('sw_cython', ['Sequencing/sw_cython.pyx'], include_dirs=include_dirs),
@@ -17,4 +18,13 @@ setup(
     ext_package='Sequencing',
     ext_modules=ext_modules,
     cmdclass={'build_ext': build_ext},
+    packages=['Sequencing',
+              'Sequencing/Visualize',
+              'Sequencing/Visualize/interactive',
+             ],
+    package_data={'Sequencing/Visualize/interactive': ['*.coffee',
+                                                       'example_df.txt',
+                                                       '*.json',
+                                                      ],
+                 },
 )
