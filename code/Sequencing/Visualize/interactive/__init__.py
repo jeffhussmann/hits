02ac252b1b8f62d507623b22aeb243639fe94535
Jeff Hussmann <jeff.hussmann@gmail.com>
2017-03-12 14:24:12 -0700

    adds initial_selection support to hists and cleans up code

--- code/Sequencing/Visualize/interactive/__init__.py
+++ code/Sequencing/Visualize/interactive/__init__.py
@@ -304,6 +304,8 @@ def scatter(df=None,
     scatter.selection_glyph.line_color = None
     scatter.nonselection_glyph.line_color = None
 
+    # Make marginal histograms.
+
     histogram_source = bokeh.models.ColumnDataSource(name='histogram_source')
     histogram_source.data = {
         'bins_left': bins[:-1],
@@ -333,19 +335,26 @@ def scatter(df=None,
                                       ),
     }
 
+    initial_xs = df[x_name].iloc[initial_indices]
+    initial_x_counts, _ = np.histogram(initial_xs, bins)
+    
+    initial_ys = df[y_name].iloc[initial_indices]
+    initial_y_counts, _ = np.histogram(initial_ys, bins)
+
     histogram_source.data['x_all'] = histogram_source.data[x_name]
     histogram_source.data['y_all'] = histogram_source.data[y_name]
-    histogram_source.data['x_selected'] = histogram_source.data['zero']
-    histogram_source.data['y_selected'] = histogram_source.data['zero']
+    histogram_source.data['x_selected'] = initial_x_counts
+    histogram_source.data['y_selected'] = initial_y_counts
 
+    initial_hist_alpha = 0.1 if len(initial_indices) > 0 else 0.2
     hist_figs['top'].quad(left='bins_left', right='bins_right',
                           bottom='zero', top='x_all',
                           source=histogram_source,
                           color='black',
-                          alpha=0.1,
+                          alpha=initial_hist_alpha,
                           line_color=None,
+                          name='hist_x_all',
                          )
-    hist_figs['top'].y_range = bokeh.models.Range1d(start=0, end=max_count, bounds='auto')
 
     hist_figs['top'].quad(left='bins_left', right='bins_right',
                           bottom='zero', top='x_selected',
@@ -359,11 +368,11 @@ def scatter(df=None,
                             left='zero', right='y_all',
                             source=histogram_source,
                             color='black',
-                            alpha=0.1,
+                            alpha=initial_hist_alpha,
                             line_color=None,
+                            name='hist_y_all',
                            )
-    hist_figs['right'].x_range = bokeh.models.Range1d(start=0, end=max_count, bounds='auto')
-
+    
     hist_figs['right'].quad(top='bins_left', bottom='bins_right',
                             left='zero', right='y_selected',
                             source=histogram_source,
@@ -371,6 +380,10 @@ def scatter(df=None,
                             alpha=0.8,
                             line_color=None,
                            )
+    
+    hist_range_kwargs = dict(start=0, end=max_count, bounds='auto')
+    hist_figs['top'].y_range = bokeh.models.Range1d(**hist_range_kwargs)
+    hist_figs['right'].x_range = bokeh.models.Range1d(**hist_range_kwargs)
 
     for hist_fig in hist_figs.values():
         hist_fig.outline_line_color = None
