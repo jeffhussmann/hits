dc7c359fe197a513b05906757d608fa3ac6c3b9f
Jeff Hussmann <jeff.hussmann@gmail.com>
2017-12-29 11:57:48 -0800

    adds support for indexed adapters

--- code/Sequencing/adapters.py
+++ code/Sequencing/adapters.py
@@ -35,13 +35,13 @@ tru_seq_R2_rc = 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC'
 P5_rc = 'TCTCGGTGGTCGCCGTATCATT'
 P7_rc = 'ATCTCGTATGCCGTCTTCTGCTTG'
 
-def build_before_adapters(I7_sequence='', primer_type='tru_seq', just_primers=False):
+def build_before_adapters(I7_sequence='', primer_type='tru_seq', just_primers=False, R1_index='', R2_index=''):
     if just_primers:
-        before_R1 = primers[primer_type]['R1']
-        before_R2 = primers[primer_type]['R2']
+        before_R1 = primers[primer_type]['R1'] + R1_index
+        before_R2 = primers[primer_type]['R2'] + R2_index
     else:
-        before_R1 = flow_cell['P5'] + primers[primer_type]['R1']
-        before_R2 = flow_cell['P7'] + utilities.reverse_complement(I7_sequence) + primers[primer_type]['R2']
+        before_R1 = flow_cell['P5'] + primers[primer_type]['R1'] + R1_index
+        before_R2 = flow_cell['P7'] + utilities.reverse_complement(I7_sequence) + primers[primer_type]['R2'] + R2_index
 
     return before_R1, before_R2
 
