fb4f5c15fec61ff9e7ddf69973b3c173683ef59c
Jeff Hussmann <jeff.hussmann@gmail.com>
2018-05-22 19:33:43 -0700

    removes izip

--- code/Sequencing/Visualize/__init__.py
+++ code/Sequencing/Visualize/__init__.py
@@ -1,5 +1,4 @@
 from . import define_igv_colors
-import itertools
 import scipy.stats
 import numpy as np
 import matplotlib
@@ -341,10 +340,11 @@ def label_scatter_plot(ax, xs, ys, labels,
         starting_labels = []
     bboxes = [label.get_window_extent() for label in starting_labels]
 
-    tuples = itertools.izip(xs[to_label],
-                            ys[to_label],
-                            labels[to_label],
-                            )
+    tuples = zip(xs[to_label],
+                 ys[to_label],
+                 labels[to_label],
+                )
+
     for x, y, label in tuples:
         distance = initial_distance
         text, bbox, coords = attempt_text(x, y, label, distance)
