e613656de7b4606fcc98739a419aa63eab4261e7
jeffhussmann <jeff.hussmann@gmail.com>
2020-04-06 22:11:33 -0700

    bumps version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 
 setup(
     name='hits',
-    version='0.0.7',
+    version='0.1.1',
 
     author='Jeff Hussmann',
     author_email='jeff.hussmann@gmail.com',
