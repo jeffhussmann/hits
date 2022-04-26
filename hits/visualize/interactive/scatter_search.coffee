3b69594edb60e6615694997e499e4e9509b9b569
Jeff Hussmann <jeff.hussmann@gmail.com>
2022-04-26 18:19:37 -0400

    adds ability to supply multiple |-separated search queries

--- hits/visualize/interactive/scatter_search.coffee
+++ hits/visualize/interactive/scatter_search.coffee
@@ -2,17 +2,21 @@ column_names = {column_names}
 
 is_case_sensitive = case_sensitive.active.length > 0
 if not is_case_sensitive
-    query = search_input.value.toLowerCase()
+    queries = search_input.value.toLowerCase().split('|')
     possibly_lowercase = (t) -> t.toString().toLowerCase()
 else
-    query = cb_obj.value
+    queries = search_input.value.split('|')
     possibly_lowercase = (t) -> t
 
+matches_any_query = (t) -> 
+    matches_query = (query) -> possibly_lowercase(t).indexOf(query) > -1 
+    queries.some(matches_query)
+
 all_matches = []
-if query != ''
+if search_input.value != ''
     for column in column_names
         targets = scatter_source.data[column]
-        matches = (i for t, i in targets when possibly_lowercase(t).indexOf(query) > -1 and i not in all_matches)
-        all_matches.push matches...
+        matches_from_column = (i for t, i in targets when matches_any_query(t) and i not in all_matches)
+        all_matches.push matches_from_column...
 
 scatter_source.selected.indices = all_matches
--- hits/visualize/interactive/scatter_search.js
+++ hits/visualize/interactive/scatter_search.js
@@ -1,4 +1,4 @@
-var all_matches, column, column_names, i, is_case_sensitive, j, len, matches, possibly_lowercase, query, t, targets,
+var all_matches, column, column_names, i, is_case_sensitive, j, len, matches_any_query, matches_from_column, possibly_lowercase, queries, t, targets,
   indexOf = [].indexOf;
 
 column_names = {column_names};
@@ -6,35 +6,43 @@ column_names = {column_names};
 is_case_sensitive = case_sensitive.active.length > 0;
 
 if (!is_case_sensitive) {
-  query = search_input.value.toLowerCase();
+  queries = search_input.value.toLowerCase().split('|');
   possibly_lowercase = function(t) {
     return t.toString().toLowerCase();
   };
 } else {
-  query = cb_obj.value;
+  queries = search_input.value.split('|');
   possibly_lowercase = function(t) {
     return t;
   };
 }
 
+matches_any_query = function(t) {
+  var matches_query;
+  matches_query = function(query) {
+    return possibly_lowercase(t).indexOf(query) > -1;
+  };
+  return queries.some(matches_query);
+};
+
 all_matches = [];
 
-if (query !== '') {
+if (search_input.value !== '') {
   for (j = 0, len = column_names.length; j < len; j++) {
     column = column_names[j];
     targets = scatter_source.data[column];
-    matches = (function() {
+    matches_from_column = (function() {
       var k, len1, results;
       results = [];
       for (i = k = 0, len1 = targets.length; k < len1; i = ++k) {
         t = targets[i];
-        if (possibly_lowercase(t).indexOf(query) > -1 && indexOf.call(all_matches, i) < 0) {
+        if (matches_any_query(t) && indexOf.call(all_matches, i) < 0) {
           results.push(i);
         }
       }
       return results;
     })();
-    all_matches.push(...matches);
+    all_matches.push(...matches_from_column);
   }
 }
 
