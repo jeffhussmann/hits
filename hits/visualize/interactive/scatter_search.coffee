f5613732f2740b7825d1a792aee57fc7f6f0bd7a
Jeff Hussmann <jeff.hussmann@gmail.com>
2023-05-05 13:05:41 -0400

    add "&" support to scatter search

--- hits/visualize/interactive/scatter_search.coffee
+++ hits/visualize/interactive/scatter_search.coffee
@@ -1,22 +1,36 @@
 column_names = {column_names}
 
+search_input = search_input.value
+
 is_case_sensitive = case_sensitive.active.length > 0
 if not is_case_sensitive
-    queries = search_input.value.toLowerCase().split('|')
+    search_input = search_input.toLowerCase()
     possibly_lowercase = (t) -> t.toString().toLowerCase()
 else
-    queries = search_input.value.split('|')
     possibly_lowercase = (t) -> t
 
-matches_any_query = (t) -> 
-    matches_query = (query) -> possibly_lowercase(t).indexOf(query) > -1 
-    queries.some(matches_query)
+matches_query = (target) ->
+    (query) -> possibly_lowercase(target).indexOf(query) > -1 
+
+if search_input.indexOf('|') > -1
+    queries = search_input.split('|')
+
+    matches_relevant_query = (target) -> queries.some(matches_query(target))
+
+else if search_input.indexOf('&') > -1
+    queries = search_input.split('&')
+
+    matches_relevant_query = (target) -> queries.every(matches_query(target))
+
+else
+    matches_relevant_query = (target) -> matches_query(target)(search_input)
+
 
 all_matches = []
 if search_input.value != ''
     for column in column_names
         targets = scatter_source.data[column]
-        matches_from_column = (i for t, i in targets when matches_any_query(t) and i not in all_matches)
+        matches_from_column = (i for target, i in targets when matches_relevant_query(target) and i not in all_matches)
         all_matches.push matches_from_column...
 
 scatter_source.selected.indices = all_matches
--- hits/visualize/interactive/scatter_search.js
+++ hits/visualize/interactive/scatter_search.js
@@ -1,29 +1,44 @@
-var all_matches, column, column_names, i, is_case_sensitive, j, len, matches_any_query, matches_from_column, possibly_lowercase, queries, t, targets,
+var all_matches, column, column_names, i, is_case_sensitive, j, len, matches_from_column, matches_query, matches_relevant_query, possibly_lowercase, queries, search_input, target, targets,
   indexOf = [].indexOf;
 
 column_names = {column_names};
 
+search_input = search_input.value;
+
 is_case_sensitive = case_sensitive.active.length > 0;
 
 if (!is_case_sensitive) {
-  queries = search_input.value.toLowerCase().split('|');
+  search_input = search_input.toLowerCase();
   possibly_lowercase = function(t) {
     return t.toString().toLowerCase();
   };
 } else {
-  queries = search_input.value.split('|');
   possibly_lowercase = function(t) {
     return t;
   };
 }
 
-matches_any_query = function(t) {
-  var matches_query;
-  matches_query = function(query) {
-    return possibly_lowercase(t).indexOf(query) > -1;
+matches_query = function(target) {
+  return function(query) {
+    return possibly_lowercase(target).indexOf(query) > -1;
   };
-  return queries.some(matches_query);
 };
+
+if (search_input.indexOf('|') > -1) {
+  queries = search_input.split('|');
+  matches_relevant_query = function(target) {
+    return queries.some(matches_query(target));
+  };
+} else if (search_input.indexOf('&') > -1) {
+  queries = search_input.split('&');
+  matches_relevant_query = function(target) {
+    return queries.every(matches_query(target));
+  };
+} else {
+  matches_relevant_query = function(target) {
+    return matches_query(target)(search_input);
+  };
+}
 
 all_matches = [];
 
@@ -35,8 +50,8 @@ if (search_input.value !== '') {
       var k, len1, results;
       results = [];
       for (i = k = 0, len1 = targets.length; k < len1; i = ++k) {
-        t = targets[i];
-        if (matches_any_query(t) && indexOf.call(all_matches, i) < 0) {
+        target = targets[i];
+        if (matches_relevant_query(target) && indexOf.call(all_matches, i) < 0) {
           results.push(i);
         }
       }
